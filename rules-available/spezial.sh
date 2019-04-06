8e6225e0601a0d2c72210b2a4d0b46d54a2088e0
Wutze <micro@mail.home>
2019-04-06 16:10:02 +0200

    Wichtiger Eintrag, der Probleme mit NETFLIX zum Beispiel behebt.

---
+++ rules-available/spezial.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#
+# Simple Firewall-Script with iptables
+# only IPv4
+#
+# (c) by Wutze 2006-18 Version 3.0
+#
+# This file is copyright under the latest version of the EUPL.
+# Please see LICENSE file for your rights under this license.
+# Version 1.x
+#
+# Twitter -> @HuWutze
+#
+
+
+##
+# Falls es Probleme gibt mit Netflix direkt im Webbrowser auf dem PC oder Fernseher gibt,
+# angeblich sei keine Verbindung zum Internet möglich, dann fehlt diese eine Zeile.
+
+#$FW -t mangle -o ppp0 --insert FORWARD 1 -p tcp --tcp-flags SYN,RST SYN -m tcpmss --mss 1400:65495 -j TCPMSS --clamp-mss-to-pmtu
+$FW -t mangle -o $DEV_EXTERN --insert FORWARD 1 -p tcp --tcp-flags SYN,RST SYN -m tcpmss --mss 1400:65495 -j TCPMSS --clamp-mss-to-pmtu
